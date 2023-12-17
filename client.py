7dfec4c17b37cf99f5db3b73401d34ca8f65467e
GalinaBoreal <galina_ver@mail.ru>
2023-12-17 12:26:09 +0500

    add and fix

--- client.py
+++ client.py
@@ -3,43 +3,53 @@ import requests
 # Создать объявление
 response = requests.post(
     "http://127.0.0.1:5000/advt",
-    json={"title": "first", "description": "A cat in gloves catches no mice.", "owner": "Alex"},
+    json={
+        "title": "first",
+        "description": "A cat in gloves catches no mice.",
+        "owner": "Alex"},
 )
 print(f'Сооздание объявления. Статус: {response.status_code}.\n{response.text}')
 
 # Получить объявление
-response = requests.get(
-    "http://127.0.0.1:5000/advt/4",
-)
+response = requests.get("http://127.0.0.1:5000/advt/1")
 print(f'Получение объявления. Статус: {response.status_code}.\n{response.text}')
 
 # редактирование объявления
 
-response = requests.patch("http://127.0.0.1:5000/advt/4",
-                          json={"title": "first", "description": "A watched pot never boils.", "owner": "Alex"}
+response = requests.patch(
+    "http://127.0.0.1:5000/advt/1",
+    json={
+        "title": "first",
+        "description": "A watched pot never boils.",
+        "owner": "Alex"}
                           )
 print(f'Изменение объявления. Статус: {response.status_code}.\n{response.text}')
 
 # Получить объявление
-response = requests.get("http://127.0.0.1:5000/advt/4")
+response = requests.get("http://127.0.0.1:5000/advt/1")
 print(f'Получение объявления. Статус: {response.status_code}.\n{response.text}')
 
 # удаление объявления
 response = requests.delete(
-    "http://127.0.0.1:5000/advt/4",
+    "http://127.0.0.1:5000/advt/1",
 )
 print(f'Удаление объявления. Статус: {response.status_code}.\n{response.text}')
 
 # Создать не проходящее валидацию объявление
 response = requests.post(
     "http://127.0.0.1:5000/advt",
-    json={"title": "f", "description": "A cat in gloves catches no mice.", "owner": "Alex"},
+    json={
+        "title": "f",
+        "description": "A cat in gloves catches no mice.",
+        "owner": "Alex"},
 )
-print(f'Сооздание объявления с однобуквенным заголовком. Статус: {response.status_code}.\n{response.text}')
+print(f'Сооздание объявления с однобуквенным заголовком. '
+      f'Статус: {response.status_code}.\n{response.text}')
 
 # Создать не проходящее валидацию объявление
 response = requests.post(
     "http://127.0.0.1:5000/advt",
     json={"title": "fi"},
 )
-print(f'Сооздание объявления без описания. Статус: {response.status_code}.\n{response.text}')
+print(f'Сооздание объявления без описания. '
+      f'Статус: {response.status_code}.\n{response.text}')
